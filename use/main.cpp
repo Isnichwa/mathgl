c700022b4669815ae24938714926ef2251644551
Isnichwa <58815086+Isnichwa@users.noreply.github.com>
2020-04-29 11:47:11 +0200

    first

---
+++ use/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  use
+//
+//  Created by Isnichwa on 29.04.20.
+//  Copyright © 2020 Isnichwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
